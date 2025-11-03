db5fce5075998657a8e2e90bfdb8d10266bb1310
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2025-11-03 19:21:34 +0300

    feat: add main functionality

--- virtual-music-kit/src/js/script.js
+++ virtual-music-kit/src/js/script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', initApp);
+
 function createElement(mainOptions, otherAttributes = {}, children = []) {
   const { tag = 'div', text = '', classes = [] } = mainOptions;
 
@@ -75,11 +77,147 @@ function createControls() {
   return controls;
 }
 
+const getDataKeys = () => {
+  const pianoKeys = document.querySelectorAll('.piano__key');
+  const dataKeys = [...pianoKeys].map((pianoKey) => pianoKey.dataset.key);
+
+  return dataKeys;
+};
+
+function addPianoKeysEventListener() {
+  let isPianoKeyActive = false;
+
+  document.addEventListener('keydown', (event) => {
+    if (event.target.tagName === 'INPUT') {
+      return;
+    }
+    if (isPianoKeyActive) {
+      return;
+    }
+
+    isPianoKeyActive = true;
+
+    const pianoKey = document.querySelector(`[data-key="${event.code[event.code.length - 1]}"]`);
+    if (pianoKey) {
+      pianoKey.classList.add('active');
+      playAudio(event.code[event.code.length - 1]);
+    }
+  });
+
+  document.addEventListener('keyup', (event) => {
+    isPianoKeyActive = false;
+    const pianoKey = document.querySelector(`[data-key="${event.code[event.code.length - 1]}"]`);
+    if (pianoKey) {
+      pianoKey.classList.remove('active');
+    }
+  });
+}
+
+function addPianoClickEventListener() {
+  document.addEventListener('mousedown', (event) => {
+    if (
+      event.target.classList.contains('piano__key') ||
+      event.target.classList.contains('piano__edit-area')
+    ) {
+      if (event.target.tagName === 'INPUT') {
+        return;
+      }
+
+      playAudio(event.target.dataset.key || event.target.closest('.piano__key').dataset.key);
+    }
+  });
+}
+
+function changeKeyData() {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && event.target.classList.contains('piano__input')) {
+      const pianoInput = event.target;
+      const pianoInputValue = pianoInput.value.toUpperCase();
+      const pianoEditArea = pianoInput.closest('.piano__edit-area');
+      const pianoKey = pianoInput.closest('.piano__key');
+      const pianoLabel = pianoEditArea.querySelector('.piano__label');
+
+      const dataKeys = getDataKeys();
+
+      if (
+        (dataKeys.includes(pianoInputValue) && pianoInputValue !== pianoKey.dataset.key) ||
+        !/[A-Z]/.test(pianoInputValue)
+      ) {
+        pianoEditArea.classList.add('error');
+        pianoInput.value = pianoKey.dataset.key;
+        pianoInput.focus();
+        pianoInput.select();
+      } else {
+        pianoKey.dataset.key = pianoInputValue;
+        pianoLabel.textContent = pianoInputValue;
+
+        if (event.target.matches('.piano__input')) {
+          event.target.closest('.piano__edit-area').classList.remove('edit-mode');
+          event.target.closest('.piano__edit-area').classList.remove('error');
+        }
+      }
+    }
+  });
+}
+
+function addPianoEditEventListener() {
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('.piano__edit')) {
+      event.target.closest('.piano__edit-area').classList.toggle('edit-mode');
+      event.target.closest('.piano__edit-area').classList.remove('error');
+
+      const pianoInput = event.target.previousElementSibling;
+      pianoInput.focus();
+      pianoInput.select();
+    }
+  });
+}
+
 function initApp() {
   const controls = createControls();
   const piano = createPiano();
 
   document.body.append(controls, piano);
+
+  addPianoClickEventListener();
+  addPianoKeysEventListener();
+  addPianoEditEventListener();
+  changeKeyData();
 }
 
-document.addEventListener('DOMContentLoaded', initApp);
+const audioContext = new AudioContext();
+
+function playAudio(objKey) {
+  const pianoKey = document.querySelectorAll('.piano__key');
+
+  const obj = {
+    [pianoKey[0].dataset.key]: 200,
+    [pianoKey[1].dataset.key]: 300,
+    [pianoKey[2].dataset.key]: 400,
+    [pianoKey[3].dataset.key]: 500,
+    [pianoKey[4].dataset.key]: 600,
+    [pianoKey[5].dataset.key]: 700,
+    [pianoKey[6].dataset.key]: 800,
+    [pianoKey[7].dataset.key]: 900,
+  };
+  const frequency = obj[objKey];
+
+  const oscillator = audioContext.createOscillator();
+  const gain = audioContext.createGain();
+
+  oscillator.type = 'triangle';
+  oscillator.frequency.value = frequency;
+
+  gain.gain.value = 0.2;
+
+  oscillator.connect(gain);
+  gain.connect(audioContext.destination);
+
+  oscillator.start();
+
+  gain.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+
+  setTimeout(() => {
+    oscillator.stop();
+  }, 500);
+}
